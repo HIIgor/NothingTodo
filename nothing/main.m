72b0a7ae66f817f3439d0fd902bfe0887b0f72c4
HiIgor <xiangyaguo@renrenche.com>
2018-11-14 11:09:59 +0800

    Initial Commit

---
+++ nothing/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  nothing
+//
+//  Created by 向亚国 on 2018/11/14.
+//  Copyright © 2018年 HiIgor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
